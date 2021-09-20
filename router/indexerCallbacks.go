12d81a7a743331592b7d33514f5527a938f326ed
Eduard S <edu@aragonlabs.com>
2021-09-20 11:19:01 +0200

    Fix block.Height appHash disparity
    Don't store the block request header in the state.  Previously the block
    request header was stored in the state.  I found this confusing because then
    the state hash is updated at every block even if there are no transactions that
    update the state, and only the height and timestamp were ever queried.
    
    Add startBlockTimestamp in BaseApplication so that it can be consumed by the
    scrutinizer (previously this was stored in the header in the state).
    
    Store the last commited block height in the NoState of the StateDB, so that on
    init we can query the height of the last stored height (required for Tendermint
    Info method).  Also keep the current block in a variable in State.
    
    Move State RWMutex into a wrapper with the Tx to make it explicit that
    only the State.Tx requires locking.

--- router/indexerCallbacks.go
+++ router/indexerCallbacks.go
@@ -15,8 +15,12 @@ import (
 func (r *Router) getStats(request RouterRequest) {
 	var err error
 	stats := new(api.VochainStats)
-	stats.BlockHeight = r.vocapp.Height()
-	stats.BlockTimeStamp = int32(r.vocapp.State.Header(true).Timestamp)
+	height := r.vocapp.Node.BlockStore().Height()
+	stats.BlockHeight = uint32(height)
+	block := r.vocapp.Node.BlockStore().LoadBlockMeta(height)
+	if block != nil {
+		stats.BlockTimeStamp = int32(block.Header.Time.Unix())
+	}
 	stats.EntityCount = int64(r.Scrutinizer.EntityCount())
 	if stats.EnvelopeCount, err = r.Scrutinizer.GetEnvelopeHeight([]byte{}); err != nil {
 		r.SendError(request, fmt.Sprintf("could not count vote envelopes: (%s)", err))
